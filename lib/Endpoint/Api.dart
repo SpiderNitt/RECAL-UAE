839baf2c53fb2b7543721e998c29ada3cc334e4a
nsi319 <sairam.nareniyer@gmail.com>
2020-09-02 23:47:48 +0530

    added Api Endpoints

---
+++ lib/Endpoint/Api.dart
@@ -0,0 +1,37 @@
+class Api {
+
+  static final String host = "https://delta.nitt.edu/recal-uae/api/";
+  static final String checkLogin = "auth/check_login";
+  static final String login = "auth/login";
+  static final String feedbackMessage = "feedback/send";
+  static final String chapterCore = "chapter/core/";
+  static final String chapterVisionMission = "chapter/";
+  static final String writeAdmin = "employment/write_admin";
+  static final String getUser = "users/profile/";
+  static final String updateuser = "users/update/";
+  static final String addFile = "users/add_file/";
+  static final String logout = "auth/logout/";
+  static final String alumniPlaced = "employment/alumni_placed";
+  static final String businessMembers = "business/members/";
+  static final String addBranch = "branch/add/";
+  static final String linkedinProfile = "employment/linked_profiles";
+  static final String marketSurvey = "employment/market_survey";
+  static final String allUsers = "users/all_users/";
+  static final String mentorGroups = "mentor_group/groups";
+  static final String getNotification = "notifications/get_notification";
+  static final String getAllNotifications = "notifications/";
+  static final String getPosition = "employment/positions";
+  static final String seekGuidance = "employment/seek_guidance";
+  static final String writeResume = "employment/write_resume";
+  static final String writeMentor = "employment/write_mentor";
+  static final String getAchievements = "achievements/";
+  static final String getAccount = "events/accounts/";
+  static final String getAllAccounts = "events/all_accounts/";
+  static final String getSocialMedia = "events/social_media/";
+  static final String getManage = "events/manage/";
+  static final String getSponsors = "events/sponsors/";
+  static final String getFelicitations = "events/felicitations/";
+  static final String getAllEvents = "events/all_events/";
+  static final String getAttendees = "event/attendees/";
+
+}
